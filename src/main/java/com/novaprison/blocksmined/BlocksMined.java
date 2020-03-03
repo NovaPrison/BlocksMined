0e541b99da6237d293d28944ea953ef17f04fe0c
BSFishy <mattprovost6@gmail.com>
2020-03-03 17:09:23 -0600

    Quality-of-life changes

--- src/main/java/com/novaprison/blocksmined/BlocksMined.java
+++ src/main/java/com/novaprison/blocksmined/BlocksMined.java
@@ -4,20 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Hashtable;
-import java.util.Set;
+import java.util.Map;
 
 public final class BlocksMined extends JavaPlugin implements Listener {
 
     private Hashtable<String, Integer> blocks = new Hashtable<>();
-    private Set<String> keys = blocks.keySet();
 
     @Override
     public void onEnable() {
@@ -33,16 +30,13 @@ public final class BlocksMined extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (label.equalsIgnoreCase("bm")) {
-        	if (args.length != 1) {
-        		sender.sendMessage(ChatColor.RED + "Expected 1 argument.");
-			} else {
-				sender.sendMessage(ChatColor.YELLOW + args[0] + " has mined " + ChatColor.BOLD + ChatColor.AQUA + blocks.get(args[0]) + ChatColor.YELLOW + " blocks.");
-			}
+            String player = args.length == 1 ? args[0] : sender.getName();
+            sender.sendMessage(ChatColor.YELLOW + player + " has mined " + ChatColor.BOLD + ChatColor.AQUA + (blocks.containsKey(player) ? blocks.get(player) : 0) + ChatColor.YELLOW + " blocks.");
 
             return true;
         } else if (label.equalsIgnoreCase("bmt")) {
-            for (String k : keys) {
-                sender.sendMessage(blocks.get(k) + "\n");
+            for (Map.Entry<String, Integer> k : blocks.entrySet()) {
+                sender.sendMessage(ChatColor.YELLOW + k.getKey() + ": " + ChatColor.BOLD + ChatColor.AQUA + k.getValue());
             }
 
             return true;
@@ -52,21 +46,10 @@ public final class BlocksMined extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onLogin(PlayerLoginEvent event) {
-        Player play = event.getPlayer();
-
-        if (!play.hasPlayedBefore()) {
-            blocks.put(play.getName(), 0);
-        }
-    }
-
-    @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        Player play = event.getPlayer();
-        String name = play.getName();
-        int bm = blocks.get(name);
-        blocks.remove(name);
-        bm++;
-        blocks.put(name, bm);
+        String name = event.getPlayer().getName();
+        int previous = blocks.containsKey(name) ? blocks.get(name) : 0;
+
+        blocks.put(name, previous + 1);
     }
 }
